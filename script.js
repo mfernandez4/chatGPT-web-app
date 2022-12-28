4026924b50deb9d5bc7c4b672a17718bd7002ef9
mark-fernandez <9023627+mfernandez4@users.noreply.github.com>
2022-12-28 13:36:20 -0800

    updated script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ const handleSubmit = async (e) => {
   loader(messageElement);
 
   // send the message to the server -> chatGPT response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://gpt-server.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
